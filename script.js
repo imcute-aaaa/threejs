6cbf0aec1c1aed70504bfd1158dd73059c74acc1
18001767679 <65285239+18001767679@users.noreply.github.com>
2021-02-12 15:33:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function blockTex(t){
 	}
 	return new THREE.CubeTextureLoader().setPath("textures/").load(a);
 }
-scene.add(new THREE.Mesh(new THREE.BoxGeometry(),new THREE.MeshBasicMaterial({color:0x739783,map:blockTex("grass")})));
+scene.add(new THREE.Mesh(new THREE.BoxGeometry(),new THREE.MeshBasicMaterial({map:blockTex("gras")})));
 camera.position.z = 5;
 const tiles=[[[]]];
 function move(k,x,y,z){
