d5bb37f7228da63a9f9b7bc0e4509f5fb50b68e8
18001767679 <65285239+18001767679@users.noreply.github.com>
2021-02-14 20:42:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function processBU(){
 	BUQ.forEach((e)=>{e.process();});
 }
 camera.position.z = 70;
-const tiles=Array(256).fill(Array(256).fill(Array(252).concat([["grass",{}],["stone",{}],["stone",{}],["bedrock",{}]])));
+const tiles=Array(16).fill(Array(16).fill(Array(12).concat([["grass",{}],["stone",{}],["stone",{}],["bedrock",{}]])));
 for(let i in tiles){
 	for(let j in tiles[i]){
 		for(let k in tiles[i][j]){
