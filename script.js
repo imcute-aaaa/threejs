7edac25c0311003c61064467d6d51f2c76be69ba
18001767679 <65285239+18001767679@users.noreply.github.com>
2021-02-14 21:23:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ class BlockUpdateEvent{
 }
 const BUQ=[];
 function processBU(){
-	BUQ.forEach((e)=>{e.process();});
+	Array(BUQ.length).forEach((e)=>{BUQ.pop().process();});
 }
 camera.position.z = 70;
 const tiles=Array(16).fill(Array(16).fill(Array(12).concat([["grass",{}],["stone",{}],["stone",{}],["bedrock",{}]])));//lag Lag LaG lAg LAg LAGgy!
