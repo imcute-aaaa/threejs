4976808e6ee1a3f8aded114180581bc5dbc6d0e5
18001767679 <65285239+18001767679@users.noreply.github.com>
2021-02-14 17:50:31 +0800

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -3,5 +3,7 @@
 //2:[top,bottom,x,y]
 //3:[top,bottom,px,nx,py,ny]
 var BTEX={
-	"grass":[1,["grass_top","dirt","grass_side"]]
+	"grass":[1,["grass_top","dirt","grass_side"]],
+	"bedrock":[0,["bedrock"]],
+	"stone":[0,["stone"]]
 }
