53156d0b9f942ca52eb09e2b6feb6cbc747c151d
18001767679 <65285239+18001767679@users.noreply.github.com>
2021-02-14 21:36:27 +0800

    Update script.js

--- script.js
+++ script.js
@@ -80,7 +80,12 @@ function processBU(){
 	}
 }
 camera.position.z = 70;
-const tiles=Array(16).fill(Array(16).fill(Array(12).concat([["grass",{}],["stone",{}],["stone",{}],["bedrock",{}]])));//lag Lag LaG lAg LAg LAGgy!
+{
+let a=()=>{return Array(12).concat([["grass",{}],["stone",{}],["stone",{}],["bedrock",{}]])}
+let b=()=>{return Array(16).map(a)}
+let c=()=>{return Array(16).map(b)}
+const tiles=c();//lag Lag LaG lAg LAg LAGgy!
+}
 for(let i=0;i<tiles.length;i++){
 	for(let j=0;j<tiles[i].length;j++){
 		for(let k=0;k<tiles[i][j].length;k++){
