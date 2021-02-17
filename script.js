19237cc3496bcc263060c4d049b8f2a111e6b728
18001767679 <65285239+18001767679@users.noreply.github.com>
2021-02-17 16:37:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -82,9 +82,8 @@ function processBU(){
 }
 camera.position.z = 70;
 {
-let a=()=>{return Array(12).fill(0).concat([["grass",{}],["stone",{}],["stone",{}],["bedrock",{}]])}
-let b=()=>{return Array(16).fill(0).map(a)}
-let c=()=>{return Array(16).fill(0).map(b)}
+let a=()=>{return Array(12).fill(0).concat([Array(16).fill(0).map(["grass",{}]),Array(16).fill(0).map(["stone",{}]),Array(16).fill(0).map(["stone",{}]),Array(16).fill(0).map(["bedrock",{}])])}
+let c=()=>{return Array(16).fill(0).map(a)}
 var tiles=c();//lag Lag LaG lAg LAg LAGgy!
 }
 for(let i=0;i<tiles.length;i++){
